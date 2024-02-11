57294fa0aa0c4940e7ddd7d1828a893ac136f83b
DiegoZC <dizuco25@gmail.com>
2024-02-11 18:15:09 +0100

    Comentarios Camera Movement Script
    11/02

--- Assets/script/CameraMovement.cs
+++ Assets/script/CameraMovement.cs
@@ -7,9 +7,9 @@ public class CameraMovement : MonoBehaviour
 {
     //private Camera _camera;
     public Transform FollowObject; //El objeto al que sigue la camara 
-    public Vector2 FollowOffset;
+    public Vector2 FollowOffset; // La distancia en la que el personaje se puede mover hasta que la camara se mueve 
     //public float FollowSpeed;
-    private Vector2 Threshold;
+    private Vector2 Threshold; // Crea los limites de la camara 
     void Start()
     {
         Threshold = CalculateThreshold();
@@ -23,15 +23,15 @@ public class CameraMovement : MonoBehaviour
         //Solamente sigue la posicion del personaje en el eje X
         transform.position = position;
     }
-    private Vector3 CalculateThreshold() 
+    private Vector3 CalculateThreshold() // Calcula el tamaño del Threshold 
     {
         Rect aspect = Camera.main.pixelRect;
-        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width/ aspect.height, Camera.main.orthographicSize);
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width/ aspect.height, Camera.main.orthographicSize); // Sacar las dimensiones de la camara
         t.y -= FollowOffset.y;
         return t;
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmos() //Dibujar un Gizmo para mostrar el tamaño del Threshold 
     {
         Gizmos.color = Color.red;
         Vector2 border = CalculateThreshold();
@@ -40,11 +40,12 @@ public class CameraMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 follow = FollowObject.transform.position;
-        float YDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+        Vector2 follow = FollowObject.transform.position; // la posicion del jugador en cada frame 
+        float YDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y); // Mantiene un resgrito de la posicion del jugador en el eje Y
 
-        Vector3 newPosition = transform.position; // 
-        if(Mathf.Abs(YDifference) >= Threshold.y) 
+        Vector3 newPosition = transform.position;
+        if(Mathf.Abs(YDifference) >= Threshold.y) //Si la diferencia en el eje Y es mayor o igual al tamaño del Threshold 
+                                                  //Mathf.Abs para que el valor obtenida siempre sea positivo
         { 
             newPosition.y = follow.y;
         }
